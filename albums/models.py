09a415a96e7ae5b0389e621c66af42369d7a9f01
Diego Bryan <thediegobryan@gmail.com>
2022-06-30 14:16:13 -0400

    setup for django music about to instal environ

---
+++ albums/models.py
@@ -0,0 +1,9 @@
+from xml.dom.minidom import AttributeList
+from django.db import models
+
+# Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    create_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
